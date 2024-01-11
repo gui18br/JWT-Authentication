46bd850b9f787ccb3c62cbb4d5c8b8b0f37edc5b
gui18br <guilhermenovaislimaa@hotmail.com>
2024-01-11 11:44:56 -0300

    feat: add SecurityFilterChain.

--- src/main/java/auth/guilherme/security/config/SecurityConfiguration.java
+++ src/main/java/auth/guilherme/security/config/SecurityConfiguration.java
@@ -2,19 +2,41 @@ package auth.guilherme.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+	
+	private JwtAuthenticationFilter jwtAuthFilter;
+	private  AuthenticationProvider authenticationProvider;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-		return ;
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf()
+			.disable()
+			.authorizeHttpRequests()
+			.requestMatchers("")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authenticationProvider(authenticationProvider)
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+			
+			return http.build();
 	}
 }
