a14388650397c6feebde289651b0910840f3ddd7
gui18br <guilhermenovaislimaa@hotmail.com>
2024-01-11 10:29:24 -0300

    feat: add generateToken method.

--- src/main/java/auth/guilherme/security/config/JwtService.java
+++ src/main/java/auth/guilherme/security/config/JwtService.java
@@ -1,12 +1,16 @@
 package auth.guilherme.security.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -23,6 +27,21 @@ public class JwtService {
 	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
 		final Claims claims = extractAllClaims(token);
 		return claimsResolver.apply(claims);
+	}
+	
+	public String generateToken(
+				Map<String, Object> extraClaims,
+				UserDetails userDetails
+			) {
+				return Jwts
+						.builder()
+						.setClaims(extraClaims)
+						.setSubject(userDetails.getUsername())
+						.setIssuedAt(new Date(System.currentTimeMillis()))
+						.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+						.signWith(getSignInKey(), SignatureAlgorithm.HS256)
+						.compact();
+		
 	}
 
 	public Claims extractAllClaims(String token) {
