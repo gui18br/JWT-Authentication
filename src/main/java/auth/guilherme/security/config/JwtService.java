29685564455b3226d83980270ee3d509cf003493
gui18br <guilhermenovaislimaa@hotmail.com>
2024-01-11 10:34:45 -0300

    feat: finished Validate JWT process.

--- src/main/java/auth/guilherme/security/config/JwtService.java
+++ src/main/java/auth/guilherme/security/config/JwtService.java
@@ -2,6 +2,7 @@ package auth.guilherme.security.config;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -29,6 +30,10 @@ public class JwtService {
 		return claimsResolver.apply(claims);
 	}
 	
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+	
 	public String generateToken(
 				Map<String, Object> extraClaims,
 				UserDetails userDetails
@@ -42,6 +47,19 @@ public class JwtService {
 						.signWith(getSignInKey(), SignatureAlgorithm.HS256)
 						.compact();
 		
+	}
+	
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+	}
+
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
 	}
 
 	public Claims extractAllClaims(String token) {
